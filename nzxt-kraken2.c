8222253e7e628efe47f0b92a83f0c23b20ad362d
Jonas Malaco <jonas@protocubo.io>
2021-03-23 10:31:03 -0300

    nzxt-kraken2: remove unnecessary include

--- nzxt-kraken2.c
+++ nzxt-kraken2.c
@@ -14,7 +14,6 @@
 #include <linux/hwmon.h>
 #include <linux/jiffies.h>
 #include <linux/module.h>
-#include <linux/spinlock.h>
 
 #define STATUS_REPORT_ID	0x04
 #define STATUS_VALIDITY		2 /* seconds; equivalent to 4 missed updates */
