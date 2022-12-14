55a11cdc6d95242743d93bba8b25dbff1a0a1d86
Herman Fries <baracoder@googlemail.com>
2022-12-14 20:46:28 +0100

    hwmon: (nzxt-smart2) Add device id
    Adding support for new device id
    1e71:2019 NZXT NZXT RGB & Fan Controller
    
    Signed-off-by: Herman Fries <baracoder@googlemail.com>
    Link: https://lore.kernel.org/r/20221214194627.135692-1-baracoder@googlemail.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/nzxt-smart2.c
+++ drivers/hwmon/nzxt-smart2.c
@@ -803,6 +803,7 @@ static const struct hid_device_id nzxt_smart2_hid_id_table[] = {
 	{ HID_USB_DEVICE(0x1e71, 0x200e) }, /* NZXT RGB & Fan Controller */
 	{ HID_USB_DEVICE(0x1e71, 0x200f) }, /* NZXT Smart Device V2 */
 	{ HID_USB_DEVICE(0x1e71, 0x2010) }, /* NZXT RGB & Fan Controller */
+	{ HID_USB_DEVICE(0x1e71, 0x2019) }, /* NZXT RGB & Fan Controller */
 	{},
 };
 
