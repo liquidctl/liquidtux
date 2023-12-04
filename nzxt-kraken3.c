bffff8c9c77ea823622bd31ef90352ea115acd3f
Aleksa Savic <savicaleksa83@gmail.com>
2023-12-04 13:34:19 +0100

    nzxt-kraken3: Fix error handling path in kraken3_probe()
    Signed-off-by: Aleksa Savic <savicaleksa83@gmail.com>

--- nzxt-kraken3.c
+++ nzxt-kraken3.c
@@ -860,13 +860,13 @@ static int kraken3_probe(struct hid_device *hdev, const struct hid_device_id *id
 	ret = hid_hw_start(hdev, HID_CONNECT_HIDRAW);
 	if (ret) {
 		hid_err(hdev, "hid hw start failed with %d\n", ret);
-		goto fail_and_stop;
+		return ret;
 	}
 
 	ret = hid_hw_open(hdev);
 	if (ret) {
 		hid_err(hdev, "hid hw open failed with %d\n", ret);
-		goto fail_and_close;
+		goto fail_and_stop;
 	}
 
 	switch (hdev->product) {
